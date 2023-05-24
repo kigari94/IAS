2b61d6f9f746c738de2fffdf76d26376b0de8fea
Mo Uhlig <mouhlig@gmail.com>
2023-05-24 12:59:55 +0200

    added facing directions

--- src/Actors/Player.gd
+++ src/Actors/Player.gd
@@ -1,5 +1,14 @@
 extends Actor
 
+export var weapon_scene_path: = "res://src/Objects/Weapon.tscn"
+var player_weapon = null
+
+func _ready() -> void:
+	var weapon_instance = load(weapon_scene_path).instance()
+	var weapon_anchor = $WeaponSpawnLocation/WeaponAnchorPoint
+	weapon_anchor.add_child(weapon_instance)
+	player_weapon = weapon_anchor.get_child(0)
+	#player_weapon.connect("attack_finished", self, "on_player_weapon_attack_finished")
 
 #export var stomp_impulse: = 600.0
 
@@ -11,17 +20,26 @@ extends Actor
 #
 func _on_DangerDetector_area_entered(body: PhysicsBody2D) -> void:
 	die()
-	#_on_Timer_timeout()
+	$Timer.start()
 
 # Godot function that handels physics calculations, it is called every frame
 func _physics_process(delta: float) -> void:
 	var is_jump_interrupted: = Input.is_action_just_released("jump") and _velocity.y < 0.0
 	var direction: = get_direction()
+	facing_direction(direction.x)
 	_velocity = calculate_move_velocity(_velocity, direction, speed, is_jump_interrupted)
 	var snap: Vector2 = Vector2.DOWN * 60.0 if direction.y == 0.0 else Vector2.ZERO
 	_velocity = move_and_slide_with_snap(
 		_velocity, snap, FLOOR_NORMAL, true
 	)
+
+func _input(event: InputEvent) -> void:
+	if event.is_action_pressed("attack"): #and _current_state != _STATES.ATTACK:
+		#_current_state = _STATES.ATTACK
+		player_weapon.attack()
+		
+#func on_player_weapon_attack_finished() -> void:
+	#_current_state = _STATES.IDLE
 
 # Calculating a Vector2, from the user inputs, as direction value for the player 
 func get_direction() -> Vector2:
@@ -45,16 +63,29 @@ func calculate_move_velocity(
 		velocity.y = 0.0
 	return velocity
 	
-# not in use for now
-#
-#func calculate_stomp_velocity(linear_velocity: Vector2, stomp_impulse: float) -> Vector2:
-#	var stomp_jump: = -speed.y if Input.is_action_pressed("jump") else -stomp_impulse
-#	return Vector2(linear_velocity.x, stomp_jump)
-#
-#
 func die() -> void:
-	queue_free()
+	set_process(false)
 
 
-# func _on_Timer_timeout():
-	# respawn()
+func _on_Timer_timeout():
+	respawn()
+	print("helloworld")
+
+func respawn() -> void :
+	set_process(false)
+	
+	
+#func on_player_weapon_attack_finished() -> void:
+	#_current_state = _STATES.IDLE
+	
+func get_move_direction() ->Vector2:
+	return Vector2(
+		Input.get_action_strength("move_right") - Input.get_action_strength("move_left"), 
+		0.0 
+	)
+func facing_direction(direction: float) -> void:
+	if direction > 0.0:
+		$WeaponSpawnLocation.scale.x = 1.0
+	elif direction < 0.0:
+		$WeaponSpawnLocation.scale.x = -1.0
+	
