5b2d9b2a7102b8cce90fa54221284f23fdcc81a0
kigari94 <36a8d9lf21x>
2023-05-28 17:42:29 +0200

    adding function for direction hint

--- src/Autoload/PlayerData.gd
+++ src/Autoload/PlayerData.gd
@@ -3,8 +3,12 @@ extends Node
 signal player_name
 
 var playerName: = "" setget set_name
+var playerOneActive: bool = false setget set_active
 
 func set_name(value: String) -> void:
 	playerName = value
 	emit_signal("playerName")
 
+func set_active(value: bool) -> void:
+	playerOneActive = value
+	emit_signal("playerOneActive")
--- src/UserInterface/UserInterface.gd
+++ src/UserInterface/UserInterface.gd
@@ -2,9 +2,10 @@ extends Control
 
 onready var scene_tree: = get_tree()
 onready var pause_overlay: ColorRect = get_node("PauseOverlay")
+onready var playerOneHint: Label = get_node("LabelRight")
+onready var playerTwoHint: Label = get_node("LabelLeft")
 
 var paused: = false setget set_paused
-
 
 
 func _unhandled_input(event: InputEvent) -> void:
@@ -16,3 +17,11 @@ func set_paused(value: bool) -> void:
 	paused = value
 	scene_tree.paused = value
 	pause_overlay.visible = value
+
+func _process(delta) -> void:
+	if PlayerData.playerOneActive:
+		playerOneHint.visible = true
+		playerTwoHint.visible = false
+	else:
+		playerTwoHint.visible = true
+		playerOneHint.visible = false
