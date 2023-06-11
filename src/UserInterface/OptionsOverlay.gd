975fe99476bc8bd704b525cde0a6499a4e53c5de
kigari94 <stefankulow@web.de>
2023-06-11 10:43:21 +0200

    adding initial volume

--- src/UserInterface/OptionsOverlay.gd
+++ src/UserInterface/OptionsOverlay.gd
@@ -1,12 +1,20 @@
 extends Control
 
 onready var music: AudioStreamPlayer = get_node("ColorRect/AudioStreamPlayer")
+
+onready var masterDefault = get_node("ColorRect/MasterVolume")
+onready var musicDefault = get_node("ColorRect/MusicVolume")
+onready var soundsDefault = get_node("ColorRect/SoundsVolume")
+
 onready var masterDisplay = get_node("ColorRect/MasterVolume/MasterDisplay")
 onready var musicDisplay = get_node("ColorRect/MusicVolume/MusicDisplay")
 onready var soundsDisplay = get_node("ColorRect/SoundsVolume/SoundsDisplay")
 
 func _ready():
 	music.play()
+	masterDefault.value = AudioServer.get_bus_volume_db(AudioServer.get_bus_index("Master"))
+	musicDefault.value = AudioServer.get_bus_volume_db(AudioServer.get_bus_index("Music"))
+	soundsDefault.value = AudioServer.get_bus_volume_db(AudioServer.get_bus_index("Sounds"))
 
 func _on_MasterVolume_value_changed(value):
 	AudioServer.set_bus_volume_db(AudioServer.get_bus_index("Master"), value)
