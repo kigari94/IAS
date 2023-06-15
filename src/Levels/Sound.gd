93c683b0caec9132884569f90dbc9acf61a713b1
Mo Uhlig <mouhlig@gmail.com>
2023-06-15 12:37:29 +0200

    prepared cutoff filter

--- src/Levels/Sound.gd
+++ src/Levels/Sound.gd
@@ -2,6 +2,7 @@ extends Node
 
 
 var backgroundSound: AudioStreamPlayer
+var effect = AudioServer.get_bus_effect(1, 0)
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -10,8 +11,10 @@ func _ready():
 	add_child(backgroundSound)
 	backgroundSound.stream = preload("res://assets/Sounds/background_music/short-chill-music-8561 (mp3cut.net).mp3")
 	backgroundSound.play()
-
+	
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
-#	pass
+#	
+#if progress > 1000 or progress < -1000:
+#	 effect.cutoff_hz = 500
