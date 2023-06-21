ab5dc5e9a689340dcb55ae2e5c604873e92061da
rezujan <k.naser@live.com>
2023-06-21 14:34:46 +0200

    Tileset (Prototyp), still buggy

---
+++ src/UserInterface/KeyboardButton.gd
@@ -0,0 +1,15 @@
+extends Button
+
+
+#var keyboard_texture = null
+
+#func _ready():
+	
+#func _pressed():
+	#print("Keyboard Button")
+	#print(keyboard_texture)
+	#find_node("Keyboard_TextureRect")
+	
+	
+
+
